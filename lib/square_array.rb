6599d83224fdafe1520ae39742d3c480306eb842
Tyler Carroll <tylercarroll1611@gmail.com>
2019-09-20 21:48:36 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,36 @@
+# def square_array(array)
+#   new_array = []
+#   counter = 0
+#   while counter < array.length do
+#     puts array[counter]
+#       #array[counter] ** 2
+#       counter += 1
+#   end
+#   new_array << array[counter] ** 2 
+# end
+
+
+
+
+
+
+
+
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
+      new_array << array[counter] ** 2 
+      counter += 1
+  end
+  new_array
 end
+
+
+
+
+
+
+
+# << shovel operator, .flatten(used with <<), .concat(),
