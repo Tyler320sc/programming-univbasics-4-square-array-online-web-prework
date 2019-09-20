7d4dad53a17ecb2c60c503e0ee421fd8d63da8ac
Tyler Carroll <tylercarroll1611@gmail.com>
2019-09-20 21:19:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -16,9 +16,18 @@ end
 
 
 
+def square_array(array)
+  new_array = []
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
+      #array[counter] ** 2
+      counter += 1
+  end
+  new_array << array[counter] ** 2 
+end
 
 
-#square_array(array)
 
 
 
